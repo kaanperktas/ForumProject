cc208d983070a184bd3948110e11b8773f388eed
Ayça Akdemir <mabeaaa@gmail.com>
2022-04-09 18:19:46 +0300

    controller layer
    Ayça Akdemir

---
+++ src/java/controller/NotificationBean.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controller;
+
+
+import dao.NotificationDao;
+import entity.Notification;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+/**
+/**
+ *
+ * @author mabea
+ */
+@Named(value = "notificationBean")
+@SessionScoped
+public class NotificationBean {
+
+    private Notification entity;
+    private NotificationDao dao;
+    private List<Notification> list;
+    
+    
+    public NotificationBean() {
+    }
+    public String getTitle(int id){
+        Notification c = this.getDao().findById(id);
+        return c.getDetail();
+    }
+    public void clear(){
+        entity = new Notification();
+    }
+    public void create(){
+        this.getDao().create(entity);
+        entity = new Notification();
+    }
+    public void delete(Notification c){
+        this.getDao().delete(c);
+        entity = new Notification();
+    }
+    public void update(){
+        this.getDao().update(entity);
+        entity = new Notification();
+    }
+    public Notification getEntity() {
+        if(entity == null) {
+            entity = new Notification();
+        }
+        return entity;
+    }
+
+    public void setEntity(Notification entity) {
+        this.entity = entity;
+    }
+
+    public NotificationDao getDao() {
+        if(dao == null) {
+            dao = new NotificationDao();
+        }
+        return dao;
+    }
+
+    public void setDao(NotificationDao dao) {
+        this.dao = dao;
+    }
+
+    public List<Notification> getList() {
+        this.list = this.getDao().getList();
+        return list;
+    }
+
+    public void setList(List<Notification> list) {
+        this.list = list;
+    }
+
+}
---
+++ src/java/controller/WriterBean.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controller;
+
+import dao.WriterDao;
+import entity.Writer;
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+/**
+ *
+ * @author mabea
+ */
+@Named(value = "writerBean")
+@SessionScoped
+public class WriterBean {
+
+    private Writer entity;
+    private WriterDao dao;
+    private List<Writer> list;
+    
+    
+    public WriterBean() {
+    }
+    public String getTitle(int id){
+        Writer c = this.getDao().findById(id);
+        return c.getName();
+    }
+    public void clear(){
+        entity = new Writer();
+    }
+    public void create(){
+        this.getDao().create(entity);
+        entity = new Writer();
+    }
+    public void delete(Writer c){
+        this.getDao().delete(c);
+        entity = new Writer();
+    }
+    public void update(){
+        this.getDao().update(entity);
+        entity = new Writer();
+    }
+    public Writer getEntity() {
+        if(entity == null) {
+            entity = new Writer();
+        }
+        return entity;
+    }
+
+    public void setEntity(Writer entity) {
+        this.entity = entity;
+    }
+
+    public WriterDao getDao() {
+        if(dao == null) {
+            dao = new WriterDao();
+        }
+        return dao;
+    }
+
+    public void setDao(WriterDao dao) {
+        this.dao = dao;
+    }
+
+    public List<Writer> getList() {
+        this.list = this.getDao().getList();
+        return list;
+    }
+
+    public void setList(List<Writer> list) {
+        this.list = list;
+    }
+
+    
+}
