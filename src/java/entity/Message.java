626e7f1a50e4441e5308a88ae6160a823685a920
esracbs <65399307+esracbs@users.noreply.github.com>
2022-04-09 16:02:35 +0300

    entityLayer
    esra cabas entity

---
+++ src/java/entity/Message.java
@@ -0,0 +1,84 @@
+
+package entity;
+
+import java.util.Date;
+
+/**
+ *
+ * @author CASPER
+ */
+public class Message {
+    private int id;
+    private String sender;
+    private String receiver;
+    private String subject;
+    private String details;
+    private Date message_date;
+    private Boolean status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getMessage_date() {
+        return message_date;
+    }
+
+    public void setMessage_date(Date message_date) {
+        this.message_date = message_date;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Message(int id, String sender, String receiver, String subject, String details, Date message_date, Boolean status) {
+        this.id = id;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.subject = subject;
+        this.details = details;
+        this.message_date = message_date;
+        this.status = status;
+    }
+}
