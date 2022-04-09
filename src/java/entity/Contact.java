f684b3c929cdaa1d3d47ec54a0159871dfc9b979
esracbs <65399307+esracbs@users.noreply.github.com>
2022-04-09 16:22:06 +0300

    EntityLayer
    EntityLayer

--- src/java/entity/Contact.java
+++ src/java/entity/Contact.java
@@ -4,10 +4,85 @@
  */
 package entity;
 
+import java.util.Date;
+
 /**
  *
  * @author CASPER
  */
 public class Contact {
+    private int id;
+    private int user_id;
+    private String mail;
+    private String subject;
+    private String message;
+    private Boolean status;
+    private Date contact_date;
+
+    public Contact(int id, int user_id, String mail, String subject, String message, Boolean status, Date contact_date) {
+        this.id = id;
+        this.user_id = user_id;
+        this.mail = mail;
+        this.subject = subject;
+        this.message = message;
+        this.status = status;
+        this.contact_date = contact_date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Date getContact_date() {
+        return contact_date;
+    }
+
+    public void setContact_date(Date contact_date) {
+        this.contact_date = contact_date;
+    }
     
 }
