93991173e3e953812fe3b7d3605deec8f6922d74
gokhansaldik <gokhansaldik@gmail.com>
2022-04-11 02:42:06 +0300

    düzenlemeler
    Gökhan Saldık

--- src/java/dao/BlogDao.java
+++ src/java/dao/BlogDao.java
@@ -1,9 +1,12 @@
-    /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
 import entity.Blog;
+import entity.Category;
+import entity.Writer;
+import java.sql.Timestamp;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -13,6 +16,8 @@ import java.util.List;
  * @author Gökhan
  */
 public class BlogDao extends DBConnection{
+    CategoryDao categoryDao;
+    WriterDao writerDao;
     public Blog findById(int id){
         Blog c = null;
         try {
@@ -21,8 +26,10 @@ public class BlogDao extends DBConnection{
             
             ResultSet rs = st.executeQuery(query);
             while(rs.next()){
+                Category k=this.getCategoryDao().findById(rs.getInt("category_id"));
+                Writer w=this.getWriterDao().findById(rs.getInt("writer_id"));
                 c = new Blog(rs.getInt("id"),rs.getString("title"),rs.getString("content"),rs.getString("image"),
-                        rs.getDate("create_date"),rs.getInt("category_id"),rs.getBoolean("status"),rs.getInt("writer_id"));
+                        rs.getTimestamp("create_date"),k,rs.getBoolean("status"),w);
             }
             
         } catch (Exception e) {
@@ -34,9 +41,9 @@ public class BlogDao extends DBConnection{
     public void create(Blog c){
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "insert into blogs(title,content,image,create_date,category_id,status,writer_id) values "
-                    + "('"+c.getTitle()+"','"+c.getContent()+"','"+c.getImage()+"','"+c.getCreate_date()+"','"+c.getCategory_id()+"','"+c.getStatus()+"'"
-                    + ",'"+c.getWriter_id()+"'";
+            String query = "insert into blogs(id,title,content,image,create_date,category_id,status,writer_id) values "
+                    + "('"+c.getId()+"','"+c.getTitle()+"','"+c.getContent()+"','"+c.getImage()+"','"+new Timestamp(System.currentTimeMillis())+"','"+c.getCategory().getId()+"','"+c.getStatus()+"'"
+                    + ",'"+c.getWriter().getId()+"')";
             st.executeUpdate(query);
         
         } catch (Exception e) {
@@ -55,12 +62,13 @@ public class BlogDao extends DBConnection{
     public void update(Blog c){
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "update blogs set title='"+c.getTitle()+"' where id="+c.getId()+"' where content="+c.getContent()
-                    +"' where image="+c.getImage()
-                    +"' where create_date="+c.getCreate_date()
-                    +"' where category_id="+c.getCategory_id()
-                    +"' where status="+c.getStatus()
-                    +"' where writer_id="+c.getWriter_id();
+            String query = "update blogs set title='"+c.getTitle()+"', content='"+c.getContent()
+                    +"', image='"+c.getImage()
+                    +"', create_date='"+c.getCreate_date()
+                    +"', category_id="+c.getCategory().getId()
+                    +", status='"+c.getStatus()
+                    +"', writer_id="+c.getWriter().getId()
+                    +" where id="+c.getId();
             st.executeUpdate(query);
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -74,8 +82,10 @@ public class BlogDao extends DBConnection{
             
             ResultSet rs = st.executeQuery(query);
             while(rs.next()){
+                Category c=this.getCategoryDao().findById(rs.getInt("category_id"));
+                Writer w=this.getWriterDao().findById(rs.getInt("writer_id"));
                 list.add(new Blog(rs.getInt("id"),rs.getString("title"),rs.getString("content"),rs.getString("image"),
-                        rs.getDate("create_date"),rs.getInt("category_id"),rs.getBoolean("status"),rs.getInt("writer_id")));
+                        rs.getTimestamp("create_date"),c,rs.getBoolean("status") ,w));
             }
             
         } catch (Exception e) {
@@ -83,4 +93,27 @@ public class BlogDao extends DBConnection{
         }
         return list;
     }
+
+    public CategoryDao getCategoryDao() {
+        if(categoryDao==null){
+            this.categoryDao=new CategoryDao();
+        }
+        return categoryDao;
+    }
+
+    public void setCategoryDao(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    public WriterDao getWriterDao() {
+        if(writerDao==null){
+            this.writerDao=new WriterDao();
+        }
+        return writerDao;
+    }
+
+    public void setWriterDao(WriterDao writerDao) {
+        this.writerDao = writerDao;
+    }
+    
 }
--- src/java/entity/Blog.java
+++ src/java/entity/Blog.java
@@ -1,8 +1,7 @@
-
 package entity;
 
+import java.sql.Timestamp;
 import java.util.Date;
-import java.util.Objects;
 
 
 public class Blog {
@@ -10,61 +9,21 @@ public class Blog {
     private String title;
     private String content;
     private String image;
-    private Date create_date;
-    private int category_id;
+    private Timestamp create_date;
+    private Category category;
     private Boolean status;
-    private int writer_id;
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 19 * hash + this.id;
-        hash = 19 * hash + Objects.hashCode(this.title);
-        hash = 19 * hash + Objects.hashCode(this.content);
-        hash = 19 * hash + Objects.hashCode(this.image);
-        hash = 19 * hash + Objects.hashCode(this.create_date);
-        hash = 19 * hash + this.category_id;
-        hash = 19 * hash + Objects.hashCode(this.status);
-        hash = 19 * hash + this.writer_id;
-        return hash;
+    private Writer writer;
+    
+    public Blog(int id, String title, String content, String image, Timestamp create_date, Category category, Boolean status, Writer writer) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.image = image;
+        this.create_date =  create_date;
+        this.category = category;
+        this.status = status;
+        this.writer = writer;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Blog other = (Blog) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (this.category_id != other.category_id) {
-            return false;
-        }
-        if (this.writer_id != other.writer_id) {
-            return false;
-        }
-        if (!Objects.equals(this.title, other.title)) {
-            return false;
-        }
-        if (!Objects.equals(this.content, other.content)) {
-            return false;
-        }
-        if (!Objects.equals(this.image, other.image)) {
-            return false;
-        }
-        if (!Objects.equals(this.create_date, other.create_date)) {
-            return false;
-        }
-        return Objects.equals(this.status, other.status);
-    }
-
     public int getId() {
         return id;
     }
@@ -104,16 +63,16 @@ public class Blog {
         return create_date;
     }
 
-    public void setCreate_date(Date create_date) {
+    public void setCreate_date(Timestamp create_date) {
         this.create_date = create_date;
     }
 
-    public int getCategory_id() {
-        return category_id;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setCategory_id(int category_id) {
-        this.category_id = category_id;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public Boolean getStatus() {
@@ -124,22 +83,13 @@ public class Blog {
         this.status = status;
     }
 
-    public int getWriter_id() {
-        return writer_id;
+    public Writer getWriter() {
+        return writer;
     }
 
-    public void setWriter_id(int writer_id) {
-        this.writer_id = writer_id;
+    public void setWriter(Writer writer) {
+        this.writer = writer;
     }
 
-    public Blog(int id, String title, String content, String image, Date create_date, int category_id, Boolean status, int writer_id) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.image = image;
-        this.create_date = create_date;
-        this.category_id = category_id;
-        this.status = status;
-        this.writer_id = writer_id;
-    }
+    
 }
