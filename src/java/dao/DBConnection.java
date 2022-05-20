32a48c97f252bbe9f5ae8158907cd9718c6f6fba
kaanperktas <98807449+kaanperktas@users.noreply.github.com>
2022-05-20 18:06:30 +0300

    Kaan
    Düzenleme yapıldı

--- src/java/dao/DBConnection.java
+++ src/java/dao/DBConnection.java
@@ -11,12 +11,8 @@ public class DBConnection {
         if (this.connection == null) {
             try {
                 Class.forName("org.postgresql.Driver");
-<<<<<<< HEAD
-                this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/forumDB", "postgres", "12345");
-=======
-                this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433/forumDB", "postgres", "12345");
->>>>>>> d9e51a2c6742cd5792715f403686e0ec8f8debb2
-                
+
+                this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/forumDB", "postgres", "12345");        
 
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
