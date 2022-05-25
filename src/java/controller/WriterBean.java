f5615fd08ea4da2e92618bb73e89cf4a0c54e25f
Ayça Akdemir <mabeaaa@gmail.com>
2022-05-25 13:09:59 +0300

    Ayça Akdemir
    Dosyalama İşlemi Tamamlandı

--- src/java/controller/WriterBean.java
+++ src/java/controller/WriterBean.java
@@ -13,60 +13,120 @@ import jakarta.faces.application.FacesMessage;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.ValidatorException;
+import jakarta.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
 import java.util.List;
+import utils.Utils;
+
 /**
  *
  * @author mabea
  */
 @Named(value = "writerBean")
 @SessionScoped
-public class WriterBean implements Serializable{
+public class WriterBean implements Serializable {
 
     private Writer entity;
     private WriterDao dao;
     private List<Writer> list;
-    
-    
+
+    private Part file1;
+    private String message;
+
+    public Part getFile1() {
+        return file1;
+    }
+
+    public void setFile1(Part file1) {
+        this.file1 = file1;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String uploadFile() throws IOException {
+        boolean file1Success = false;
+
+        if (file1 != null && file1.getSize() > 0) {
+            String fileName = Utils.getFileNameFromPart(file1);
+
+            File savedFile = new File("/internet", fileName);
+
+            try (InputStream input = file1.getInputStream()) {
+                Files.copy(input, savedFile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            file1Success = true;
+        }
+
+        if (file1Success) {
+
+            setMessage("Dosya başarıyla yüklendi");
+        } else {
+
+            setMessage("Dosya yüklenemedi. Lütfen başka bir dosya seçiniz!!");
+        }
+
+        return null;
+    }
+
     public WriterBean() {
     }
-     public boolean validateName(FacesContext context,UIComponent cmp,Object value)throws ValidatorException{
-        
+
+    public boolean validateName(FacesContext context, UIComponent cmp, Object value) throws ValidatorException {
+
         String v = (String) value;
-        
-        if(v.isEmpty()){
+
+        if (v.isEmpty()) {
             throw new ValidatorException(new FacesMessage("Name alanı boş olamaz"));
-        }
-        else if(v.length() <5){
+        } else if (v.length() < 5) {
             throw new ValidatorException(new FacesMessage("Name alanı 5 karakterden küçük olamaz"));
         }
         return true;
     }
-    public String getTitle(int id){
+
+    public String getTitle(int id) {
         Writer c = this.getDao().findById(id);
         return c.getName();
     }
-    public void clear(){
+
+    public void clear() {
         entity = new Writer();
     }
-    public void create(){
+
+    public void create() {
         this.getDao().create(entity);
         entity = new Writer();
     }
-    public void delete(Writer c){
+
+    public void delete(Writer c) {
         this.getDao().delete(c);
         entity = new Writer();
     }
-     public Writer findById(){
+
+    public Writer findById() {
         return this.getDao().findById(entity.getId());
-       
+
     }
-    public void update(){
+
+    public void update() {
         this.getDao().update(entity);
         entity = new Writer();
     }
+
     public Writer getEntity() {
-        if(entity == null) {
+        if (entity == null) {
             entity = new Writer();
         }
         return entity;
@@ -77,7 +137,7 @@ public class WriterBean implements Serializable{
     }
 
     public WriterDao getDao() {
-        if(dao == null) {
+        if (dao == null) {
             dao = new WriterDao();
         }
         return dao;
@@ -96,5 +156,4 @@ public class WriterBean implements Serializable{
         this.list = list;
     }
 
-    
 }
