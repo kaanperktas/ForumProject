e7049e0cadfc5398381e6e2a4660accb789a5d2e
kaanperktas <98807449+kaanperktas@users.noreply.github.com>
2022-05-25 12:22:39 +0300

    Kaan Perktaş
    Utils oluşturuldu.

---
+++ src/java/utils/Utils.java
@@ -0,0 +1,23 @@
+
+package utils;
+
+
+import jakarta.servlet.http.Part;
+
+public final class Utils {
+
+	private Utils() {
+	}
+
+	public static String getFileNameFromPart(Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		for (String content : partHeader.split(";")) {
+			if (content.trim().startsWith("filename")) {
+				String fileName = content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+				return fileName;
+			}
+		}
+		return null;
+	}
+
+}
