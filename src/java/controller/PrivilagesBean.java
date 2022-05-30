3bb0edc785a1a23c0e0a4cd9a43893aa0d1ce14f
gokhansaldik <gokhansaldik@gmail.com>
2022-05-30 17:31:10 +0300

    gokhansaldik
    Yetkilendirilme işleminin yapılması

---
+++ src/java/controller/PrivilagesBean.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.io.Serializable;
+import dao.PrivilegeDao;
+import entity.Privileges;
+import entity.SystemGroup;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.util.List;
+
+
+@Named
+@SessionScoped
+public class PrivilagesBean implements Serializable{
+    private Privileges entity;
+    private PrivilegeDao dao;
+    private List<Privileges> list;
+
+    public Privileges getEntity() {
+        if (this.entity==null) {
+            this.entity=new Privileges();
+        }
+        return entity;
+    }
+
+    public Privileges getPrivileges(SystemGroup sg,String module){
+        return this.getDao().getGroupPrivileges(sg,module);
+    }
+    public void clearForm(){
+        this.entity=new Privileges();
+    }
+    public void create(){
+        this.getDao().create(entity);
+        this.entity=new Privileges();
+    }
+    public void update(){
+        this.getDao().update(entity);
+        this.entity=new Privileges();
+    }
+    public void delete(){
+        this.getDao().delete(entity);
+        this.entity=new Privileges();
+    }
+    public void setEntity(Privileges entity) {
+        this.entity = entity;
+    }
+
+    public PrivilegeDao getDao() {
+        if (this.dao==null) {
+            this.dao=new PrivilegeDao();
+        }
+        return dao;
+    }
+
+    public void setDao(PrivilegeDao dao) {
+        this.dao = dao;
+    }
+
+    public List<Privileges> getList() {
+        this.list=this.getDao().readList();
+        return list;
+    }
+
+    public void setList(List<Privileges> list) {
+        this.list = list;
+    }
+    
+}
