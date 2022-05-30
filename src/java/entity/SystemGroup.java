e6d6de6efe7d6f30baad4a233ce36c0cac8c83be
Ayça Akdemir <mabeaaa@gmail.com>
2022-05-30 17:20:01 +0300

    Ayça Akdemir
    yetkilendirme için entityler oluşturuldu(systemgroup,privileges)

---
+++ src/java/entity/SystemGroup.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author CASPER
+ */
+public class SystemGroup implements Serializable{
+    private Long id;
+    private String gname;
+    private Date created;
+    private Date updated;
+
+    public SystemGroup() {
+    }
+
+    public SystemGroup(Long id, String gname, Date created, Date updated) {
+        this.id = id;
+        this.gname = gname;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGname() {
+        return gname;
+    }
+
+    public void setGname(String gname) {
+        this.gname = gname;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SystemGroup other = (SystemGroup) obj;
+        return Objects.equals(this.id, other.id);
+    }
+    
+}
